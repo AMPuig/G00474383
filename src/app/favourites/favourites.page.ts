a9a2944d1efc70d4197f8c818404062fd3533292
AMPuig <195044833+AMPuig@users.noreply.github.com>
2026-05-03 10:00:00 +0100

    Favourites page complete: add/remove movies

--- src/app/favourites/favourites.page.ts
+++ src/app/favourites/favourites.page.ts
@@ -1,20 +1,42 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { Router } from '@angular/router';
+import { Storage } from '@ionic/storage-angular';
+import {
+  IonContent, IonHeader, IonTitle, IonToolbar,
+  IonButton, IonButtons, IonIcon, IonImg, IonLabel, IonItem, IonList
+} from '@ionic/angular/standalone';
+import { addIcons } from 'ionicons';
+import { home } from 'ionicons/icons';
 
 @Component({
   selector: 'app-favourites',
   templateUrl: './favourites.page.html',
   styleUrls: ['./favourites.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [
+    CommonModule,
+    IonContent, IonHeader, IonTitle, IonToolbar,
+    IonButton, IonButtons, IonIcon, IonImg, IonLabel, IonItem, IonList
+  ]
 })
 export class FavouritesPage implements OnInit {
+  favourites: any[] = [];
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(private storage: Storage, private router: Router) {
+    addIcons({ home });
   }
 
+  async ngOnInit() {
+    await this.storage.create();
+    this.favourites = await this.storage.get('favourites') || [];
+  }
+
+  goToMovieDetails(movie: any) {
+    this.router.navigate(['/movie-details'], { state: { movie } });
+  }
+
+  goToHome() {
+    this.router.navigate(['/home']);
+  }
 }
